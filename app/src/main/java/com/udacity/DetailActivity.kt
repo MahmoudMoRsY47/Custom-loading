a11d9a726a5db4a436e9848e70a45afdfafb3cda
MahmoudMoRsY47 <MahmoudMoRsY47>
2022-09-20 16:33:11 +0200

    commit

---
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -0,0 +1,37 @@
+package com.udacity
+
+import android.app.NotificationManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_detail.*
+
+class DetailActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        setSupportActionBar(toolbar)
+
+        val notificationManager = getSystemService(NotificationManager::class.java)
+        notificationManager.cancelAll()
+
+        val fileName = intent.getStringExtra(Companion.DETAIL_ACTIVITY_INTENT_STATUS_KEY)
+        fileNameValue.text = fileName
+
+        val status = intent.getStringExtra(DETAIL_ACTIVITY_INTENT_FILENAME_KEY)
+        statusValue.text = status
+
+        button.setOnClickListener {
+            finish()
+        }
+
+    }
+
+    companion object {
+        const val DETAIL_ACTIVITY_INTENT_STATUS_KEY = "Status"
+        const val DETAIL_ACTIVITY_INTENT_FILENAME_KEY = "FileName"
+    }
+}
